11d9124e7d31528f092b70c87102803adf64290b
Skhaton23 <skhaton237@gmail.com>
2025-03-05 20:45:09 -0500

    Add files via upload

--- WordGenerator.swift
+++ WordGenerator.swift
@@ -7,65 +7,16 @@
 
 import Foundation
 
-enum WordTheme: String {
-  case normal, animals, countries
-  
-  func possibleWords(numLetters: Int) -> [String] {
-    switch self {
-    case .normal:
-      return normalWords(with: numLetters)
-    case .animals:
-      return animalWords(with: numLetters)
-    case .countries:
-      return countryWords(with: numLetters)
-    }
-  }
-  
-  func normalWords(with numLetters: Int) -> [String] {
-    if numLetters == kMinLetters {
-      return ["SEED", "GOAT", "LOAD"]
-    } else if numLetters == kMinLetters + 1 {
-      return ["GRAPE", "SMART", "PIZZA"]
-    } else if numLetters == kMinLetters + 2 {
-      return ["PENCIL", "COFFEE", "ROCKET"]
-    } else if numLetters == kMaxLetters {
-      return ["BICYCLE", "WEEKEND", "LIBRARY"]
-    }
-    return []
-  }
-  
-  func animalWords(with numLetters: Int) -> [String] {
-    if numLetters == kMinLetters {
-      return ["SEAL", "DEER", "FROG"]
-    } else if numLetters == kMinLetters + 1 {
-      return ["HORSE", "PANDA", "ZEBRA"]
-    } else if numLetters == kMinLetters + 2 {
-      return ["JAGUAR", "PELICAN", "DOLPHIN"]
-    } else if numLetters == kMaxLetters {
-      return ["ELEPHANT", "GIRAFFE", "PLATYPUS"]
-    }
-    return []
-  }
-  
-  func countryWords(with numLetters: Int) -> [String] {
-    if numLetters == kMinLetters {
-      return ["PERU", "CUBA", "MALI"]
-    } else if numLetters == kMinLetters + 1 {
-      return ["INDIA", "CHILE", "QATAR"]
-    } else if numLetters == kMinLetters + 2 {
-      return ["BELIZE", "BRAZIL", "GREECE"]
-    } else if numLetters == kMaxLetters {
-      return ["AMERICA", "GERMANY", "JAMAICA"]
-    }
-    return []
-  }
-}
-
 class WordGenerator {
-  static func generateGoalWord(with theme: WordTheme) -> [String] {
-    let numLetters = SettingsManager.shared.settingsDictionary[kNumLettersKey] as! Int
-    let randomWord = theme.possibleWords(numLetters: numLetters).randomElement()!
-    return randomWord.map { String($0) }
+  // Exercise 8: Add more possible words and return a **random** string every time `generateRandomWord` is called
+  // IMPORTANT: MAKE SURE ALL THE LETTERS IN THE NEW WORDS YOU ADD ARE CAPITALIZED (e.g. "AUDIO" instead of "audio")
+  // Tip: Look at the Array API documentation to see what method returns a random element
+  // Checkpoint: After finishing this exercise, you should now have a different goal word each time you run the app! Try printing out the selected goal word to check. If it's not working, check that you have done this exercise correctly.
+    static let possibleWords = ["TABLE", "WATER", "APPLE", "MONEY", "SPACE", "DRIVE", "DANCE", "ELEPHANT", "PIZZERIA", "CAR", "Dog", "CARROT", "Bird", "FISH", "WATERMELON", "BANANA", "ORANGE", "PEAR", "PEACH", "GRAPES", "STRAWBERRY", "BLUEBERRY", "CHERRY", "PLUM", "POMEGRANET", "PINEAPPLE"]
+    static func generateRandomWord() -> String? {
+    // START YOUR CODE HERE
+    return possibleWords.randomElement()
+    // END YOUR CODE HERE
   }
 }
 
